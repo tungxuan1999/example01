d130115c4241205280ff0e52179451333d743788
AdTekDev <nguyenngtu777@gmail.com>
2020-04-06 12:42:36 +0700

    Update vd01.py

--- vd01.py
+++ vd01.py
@@ -1,13 +1,11 @@
 import web
 urls = (
-'/input', 'index'
+'/', 'index'
 )
 class index:
-  def GET(self):
-    i = web.input(name=None)
-    return render.index(i.name)
-    
+    def GET(self):
+        return "Welcome !!!"
+
 if __name__ == "__main__":
-  app = web.application(urls, globals())
-  app.run()
-render = web.template.render('templates/')
+    app = web.application(urls, globals())
+    app.run()
