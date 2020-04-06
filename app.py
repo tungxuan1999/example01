f6a594467810931c0fdfb0a7952f0a0166d7ac34
AdTekDev <nguyenngtu777@gmail.com>
2020-04-06 11:19:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask   
+from flask import Flask, url_for   
 
 
 app = Flask(__name__)     
 
 @app.route("/")           
 def hello():              
-	return "Hello World!" 
+	return "Welcome !" 
+
+@app.route("/hello")           
+def hello():              
+	return "Hello World!"
+
 if __name__ == "__main__":
 	app.run()             
